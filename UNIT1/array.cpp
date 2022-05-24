ecccecdc2067d25cf4c6f5b16225c3b611523fba
Luis Cota <luis.f.m.c@outlook.com>
2022-05-24 12:54:31 -0500

    commit

---
+++ UNIT1/array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n, i;
+    int array[10];
+
+    cout << "insert 10 numbers: " << endl;
+
+    for (int i = 0; i < 10; i++){
+        cout << ""; cin >> array[i];
+    }
+
+    cout << "1th: " << array[0] << endl;
+    cout << "5th: " << array[4] << endl;
+    cout << "10th: " << array[9] << endl;
+
+    }
+    
+
+

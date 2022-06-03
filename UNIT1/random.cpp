1ef454f8a84e950e5aeef48471dd07a5d72158d1
Luis Cota <luis.f.m.c@outlook.com>
2022-06-03 07:19:42 -0500

    commit

--- UNIT1/random.cpp
+++ UNIT1/random.cpp
@@ -1,31 +1,80 @@
 #include <iostream>
-#include <ctime>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
 
 using namespace std;
-const int col = 11, MAXIMO = 20, row = 11;
-int arreglo[ col ][ row ] = { 0 };
 
-   
+int array[10][10];
+void generate_random();
+void repeat ();
+void maxm();
+
 int main(){
+	generate_random();
+	repeat();
+	maxm();
+	return 0;
+}
 
-    int i;
-    int j;
-    srand( time( 0 ) );
+void generate_random(){
+	srand(time(0));
+	
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 10; j++){
+			array[i][j] = rand() % 201;
+		}
+	}
+	
+	for(int i = 0; i < 10; i++){
+		cout << "\n";
+		for(int j = 0; j < 10; j++){
+			cout << setw (3)<<array[i][j] << "  ";
+		}
+	}
+}
 
-    for (int i = 0; i < col; i++ ){
-        for (j = 0; j < row; j++){
-            arreglo[ i ][ j ]= rand() % MAXIMO;
-        }
-    }
+void repeat(){
+	int maxf = 0;
+	int mostf = -1;
+	
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 10; j++){
+			int freq = 0;
+			
+			for(int k = 0; k < 10; k++){
+				for(int l = 0; l < 10; l++){
+					if(array[i][j] == array[k][l]){
+						freq++;
+					}
+				}
+			}
+			
+			if(maxf < freq){
+				maxf = freq;
+				mostf = array[i][j];
+			}
+		}
+	}
+	
+	
+	cout<<"\n";
+	cout << "\n The most repeated number is: " << mostf;
 
-     cout << "Los valores del arreglo son:" << endl;
+}
 
-    for ( i = 0; i < col; i++ ){
-        for (j = 0; j < row; j++){
-            cout <<"arreglo[" << i << "]["<<j<<"]= " << arreglo[ i ][ j ];
-            cout << "\n";
-        }
-    }
-
-return 0;
+void maxm(){
+	int max = -1;
+	
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 10; j++){
+			if(array [i][j] > max){
+				max = array[i][j];
+			}
+		}
+	}
+	
+	cout<< "\n";
+	
+	cout <<"\n The max number is: " << max << endl; 
 }

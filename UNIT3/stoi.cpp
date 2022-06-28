6cb9576a98e9fb09c6df6487ad5ce4bd92b8d624
Luis Cota <luis.f.m.c@outlook.com>
2022-06-27 22:31:13 -0500

    commit

--- UNIT3/stoi.cpp
+++ UNIT3/stoi.cpp
@@ -1,4 +1,7 @@
-
+/*
+Karina Campos
+Luis Monterrubio 
+*/
 #include<iostream>
 #include<stdio.h>
 #include<string>
@@ -10,37 +13,32 @@ int main(){
 	int cols = 2; //el numero de columnas fijas
 	string** libros; //inicializando la matriz con punteros
 	int size;
-	cout<<"Cuantos libros debe ingresar? \n";
+	cout<<"Cuantos libros debe ingresar?" << endl;
 	string entrada; //declaro la variable de entrada
 	getline(cin, entrada); //se asigna el valor ingresado
 	size = stoi(entrada); //transformar la entrada "string" a "entero"
 	libros = new string*[size]; //dasignar el numero de filas segun el usuario
-	
-	cout<<"Ingrese la siguiente informacion de los libros: \n";
+	cout<<"Ingrese la siguiente informacion de los libros: " << endl;
 	string titulo,autor;
 	
-	for(int i=0; i<size; i++){
-		
+	int i, j, position;
+	for(i = 0; i < size; i++){
+
 		libros[i] = new string[cols];
-		cout<<"\n****** Libro"<<i+1<<"********\n";
-		cout<<"titulo: ";
+		cout<<"\n****** Libro"<<i+1<<"********" << endl;
+		cout<<"Titulo: ";
 		getline(cin,titulo);
-		cout<<"autores: ";
+		cout<<"Autores: ";
 		getline(cin,autor); //se asigna el valor 
 		libros[i][0] = titulo;
 		libros[i][1] = autor;
+		cout << "Escribe el titulo del libro que deseas encontrar: "; 
+		cout << libros[i][0] << " " << *libros << libros[i][1] << "\n";
+		
 	}
-	/*encontrar libro en base a la posicion con punteros*/
 
-	int posicion ;
-	string *direcciondelibro;
+	//liberar la memoria debemos recorrer el array fila pro fila
 
-	cout << "Escribe el titulo del libro que deseas encontrar: ";
-	cin >> posicion;
-
-	direcciondelibro = libros [posicion];
-
-	cout << "El libro seleccionado es  " + *(direcciondelibro) + "  de " +  *(direcciondelibro+1)  << endl ;
 
 	system("pause");
 	return 0;

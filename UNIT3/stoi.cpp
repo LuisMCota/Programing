14feeec8c077ecf2cfdbc4c882270aee383a957a
Luis Cota <luis.f.m.c@outlook.com>
2022-07-05 11:21:47 -0500

    hello

--- UNIT3/stoi.cpp
+++ UNIT3/stoi.cpp
@@ -8,40 +8,49 @@ lUIS MONTERRUBIO
 
 using namespace std;
 
-int main(){
-	
-	int cols = 2; 
-	string** libros;
-	int size;
-	cout<<"Cuantos libros debe ingresar? \n";
+int cols = 2; 
+string** libros;
+int size;
+int x;
+
+
+int crear_matrix(){
+	cout << "Cuantos libros debe ingresar? \n";
 	string entrada; 
 	getline(cin, entrada); 
 	size = stoi(entrada); 
 	libros = new string*[size]; 
-	cout<<"Ingrese la siguiente informacion de los libros: \n";
-	string titulo,autor;
+}
+
+int rellenarMatriz(){
+	cout << "Ingrese la informacion de los libros" << endl;
+	string titulo, autor; 
 	
 	for(int i=0; i<size; i++){
 		
 		libros[i] = new string[cols];
-		cout<<"\n****** Libro"<<i+1<<"********\n";
-		cout<<"titulo: ";
+		cout<<"\n************ Libro "<<i+1<<"************\n";
+		cout<<"Title: ";
 		getline(cin,titulo);
-		cout<<"autores: ";
+		cout<<"Autors: ";
 		getline(cin,autor); 
 		libros[i][0] = titulo;
 		libros[i][1] = autor;
-	}
-
-	int posicion ;
-	string *direcciondelibro;
-	cout << "Escribe el titulo del libro que deseas encontrar: ";
-	cin >> posicion;
-	direcciondelibro = libros [posicion];
-
-	cout << "El libro seleccionado es  " + *(direcciondelibro) + "  de " +  *(direcciondelibro+1)  << endl ;
-
-	system("pause");
-	return 0;
+	}	
 }
- /* cout << str [i][0] << ' ' << *pc <<< */
+
+int printInfo(){
+	
+	cout << "Ingrese el libro que desee imprimir:" << endl;
+	cin >> x;
+
+	for (int i = 0; i < size; i++){
+		cout << libros[i][0] << " " << *libros << " " << libros[i][1] << endl;
+	}
+}
+
+int main(){
+	crear_matrix();
+	rellenarMatriz();
+	printInfo();
+}

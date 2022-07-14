b86eeb733c3308ccb45f5dfe66b2d8e226271ec0
Luis Cota <luis.f.m.c@outlook.com>
2022-07-14 13:22:23 -0500

    commit

---
+++ UNIT3/assigment.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int entrada;
+int size;
+
+using namespace std;
+
+int main(){
+
+cout << "Cuantos libros desea ingresar?" << endl;
+    string entrada;
+
+    getline(cin, entrada);
+    size = stoi(entrada);
+
+struct datos{
+	char nombre[21];
+	int year;
+	char autor[21];
+}libro[size];
+
+	FILE *fichero;
+	int i, numero = 0;
+	fichero = fopen("libros.dat", "wt");
+
+	if(fichero == NULL)
+		printf("No se pudo encontrar el libro.\n");            
+	else{
+        for (i=0; i<size; i++){
+
+            puts("NOMBRE: ");
+            scanf("%s", &libro[i].nombre);
+            fprintf(fichero,"NOMBRE: %s\n", libro[i].nombre);
+            getchar();
+
+            puts("AUTOR: ");
+            scanf("%s", &libro[i].autor);
+            fprintf(fichero,"AUTOR: %s\n", libro[i].autor);
+
+            puts("AÑO: ");
+            scanf("%d", &libro[i].year);
+            fprintf(fichero,"AÑO: %d\n\n", libro[i].year);
+            getchar();
+        }
+    }
+    fclose(fichero);
+
+        fichero = fopen("libros.dat", "rt");
+        if(fichero == NULL)
+            printf("No se pudo encontrar el libro.\n");
+        else{ 
+            do{
+                puts("Escriba el numero de fichero a leer: \n"
+                    "(Para salir, digite 0).");
+                scanf("%d", &numero);
+                for (i = 0; i < numero; i++){
+                    if (i ==(numero-1)){
+                        cout<<"\n************ Libro "<<i+1<<" ************\n";
+                        printf("NOMBRE: %s\nAUTOR: %s\nAÑO: %d\n\n", 
+                            libro[i].nombre, libro[i].autor, libro[i].year);
+                        getchar();
+                    }
+                }
+            } 
+while (numero!=0);
+            fclose(fichero);
+        }       
+return 0;
+}
